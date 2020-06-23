bdc348dd610701dfec7086cf438556e7bdc32443
Wojciech Nieweglowski <nieweglowski.wojciech@gmail.com>
2020-06-23 16:25:39 +0200

    Drobna poprawka w jednym z requestow CURL - 23.06.2020, g. 16:25

--- request_curl_scripts/patchCourseData_endCourse.bat
+++ request_curl_scripts/patchCourseData_endCourse.bat
@@ -1,3 +1,3 @@
-curl -i -X PATCH -H "Content-Type: application/json" -d "{\"completed\":\"true\"}" http://127.0.0.1:8080/osk/course_data/participant/pay_course/13
+curl -i -X PATCH -H "Content-Type: application/json" -d "{\"completed\":\"true\"}" http://127.0.0.1:8080/osk/course_data/instructor/end_course/13
 
 Pause
